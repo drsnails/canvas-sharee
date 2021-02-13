2867d7662b70932c0bad2533c77ff3f93bc87654
Stav Yaar bar <stavyaarbar@Stavs-MacBook-Pro.local>
2021-02-13 22:15:41 +0200

    second

--- js/upload-service.js
+++ js/upload-service.js
@@ -4,7 +4,7 @@
 function uploadImg(elForm, ev) {
     ev.preventDefault();
     document.getElementById('imgData').value = gElCanvas.toDataURL("image/jpeg");
-  
+
     // A function to be called if request succeeds
     function onSuccess(uploadedImgUrl) {
         uploadedImgUrl = encodeURIComponent(uploadedImgUrl)
@@ -13,35 +13,26 @@ function uploadImg(elForm, ev) {
            Share   
         </a>`
     }
-    let inputVal = elForm.querySelector('input').value
-    doUploadImg(elForm, onSuccess, inputVal);
-  }
-  
-  
-  async function doUploadImg(elForm, onSuccess, inputVal) {
+    const imgData = elForm.querySelector('input').value
+    doUploadImg(imgData, onSuccess);
+}
+
+async function doUploadImg(imgData, onSuccess) {
     const CLOUD_NAME = "webify"
     const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
-  
-    const img = new Image()
-    img.src = inputVal
-    img.onload = async ()=> {
-  
-        const formData = new FormData();
-        formData.append('file', img.src)
-        formData.append('upload_preset', 'webify');
-        try {
-            const res = await fetch(UPLOAD_URL, {
-                method: 'POST',
-                body: formData
-            })
-            const data = await res.json()
-            console.log('data', data);
-            // return data
-            onSuccess(data.secure_url)
-    
-        } catch (err) {
-            console.log(err);
-        }
+    const formData = new FormData();
+    formData.append('file', imgData)
+    formData.append('upload_preset', 'webify');
+    try {
+        const res = await fetch(UPLOAD_URL, {
+            method: 'POST',
+            body: formData
+        })
+        const data = await res.json()
+        console.log('data', data);
+        onSuccess(data.secure_url)
+
+    } catch (err) {
+        console.log(err);
     }
-  }
-  
+}
